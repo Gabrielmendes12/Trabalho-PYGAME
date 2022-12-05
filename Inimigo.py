70d27ea4c74a925a95c56122b9774d2fe0c68960
Joao Pedro Passos Ribeiro de Menezes <86020181+Mnzss1133@users.noreply.github.com>
2022-12-04 23:23:39 -0300

    Update Inimigo.py

--- Inimigo.py
+++ Inimigo.py
@@ -1,1 +1,23 @@
+import pygame
+import random
 
+class Inimigo(pygame.sprite.Sprite):
+	def __init__(self, SCREEN_WIDTH, y, sprite_sheet, scale):
+		pygame.sprite.Sprite.__init__(self)
+		#define variables
+		self.animation_list = []
+		self.frame_index = 0
+		self.update_time = pygame.time.get_ticks()
+		self.direction = random.choice([-1, 1])
+		if self.direction == 1:
+			self.flip = True
+		else:
+			self.flip = False
+      
+    #numero de sprites do charizard
+		animation_steps = 1
+		for animation in range(animation_steps):
+			image = sprite_sheet.get_image(animation, 20 ,20, scale, (0, 0, 0))
+			image = pygame.transform.flip(image, self.flip, False)
+			image.set_colorkey((0, 0, 0))
+			self.animation_list.append(image)
